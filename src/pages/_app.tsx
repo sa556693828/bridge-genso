e1073276510235ab753f097920e4cb47822a75ad
sa556693828 <sa556693828@gmail.com>
2024-03-08 21:41:33 +0800

    Env_Key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const key = process.env.ALCHEMY_API_KEY as string;
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon],
+  [alchemyProvider({ apiKey: key }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Demo",
+  projectId: "631e3d900609032a9571370557f3cef1",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
